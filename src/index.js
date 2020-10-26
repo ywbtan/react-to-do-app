9fd099faf0ca7c07cfbc21e7df86b046c6af1034
Brandon <ywbtan@Brandons-MacBook-Pro.local>
2020-10-26 17:17:16 +0800

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function Todo(props) {
+	return (
+		<div 
+			className="todo"
+			style={{ textDecoration: props.task.hasCompleted ? "line-through" : "" }}>
+				{props.task.text}
+			<div>
+				<button onClick={props.onClick}>Complete</button>
+			</div>
+		</div>
+	);
+}
+
+function TodoForm(props) {
+	const [value, setValue] = useState("");
+	
+	const handleSubmit = e => {
+		e.preventDefault();
+		if (!value) return;
+		props.addTask(value);
+		setValue(""); // sets back the input to the empty field
+	}
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<input
+				type="text"
+				className="input"
+				value={value}
+				onChange={e => setValue(e.target.value)}
+			/>
+			<input type="submit" value="Submit" />
+		</form>
+	);
+}
+
+function TodoContainer() {
+	const [todos, setTodos] = useState([]);
+
+	const addTask = newTask => {
+		const newTodos = [...todos, {
+			text: newTask,
+			hasCompleted: false,
+		}];
+		setTodos(newTodos);
+	};
+
+	const completeTask = task => {
+		task.hasCompleted = true;
+		const newTodos = [...todos];
+		setTodos(newTodos);
+	};
+
+	return (
+		<div className="app">
+			<div className="todo-list">
+				{todos.map((todo, index) => {
+					return (
+						<Todo 
+							task={todo}
+							onClick={() => completeTask(todo)}
+						/>
+					);
+				})}
+				<TodoForm addTask={(task) => addTask(task)} />
+			</div>
+		</div>
+	);
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <TodoContainer />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
